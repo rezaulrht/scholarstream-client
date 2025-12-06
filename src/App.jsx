f6840bf4dcf66f21b717a4b60698a925757ad658
rezaulkarim <rezaulrahaat@gmail.com>
2025-12-06 18:30:25 +0600

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-base-100'>
+      <h1 className='bg-primary text-base-100'>Vite + React</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
